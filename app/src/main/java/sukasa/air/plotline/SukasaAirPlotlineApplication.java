cc363d94dda7fd6b138b39fa186c80e8ad7ec764
Diablo73 <ashul2173@gmail.com>
2024-06-11 23:07:57 +0530

    initial commit for springboot application

---
+++ app/src/main/java/sukasa/air/plotline/SukasaAirPlotlineApplication.java
@@ -0,0 +1,12 @@
+package sukasa.air.plotline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SukasaAirPlotlineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SukasaAirPlotlineApplication.class, args);
+    }
+}
